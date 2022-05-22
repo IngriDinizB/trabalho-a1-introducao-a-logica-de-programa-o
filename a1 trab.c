1ae94be5a0af289ce9981b12dc6a44c3dd6dab1e
Ingrid Diniz <ingridiniz.b@gmail.com>
2022-05-22 17:20:49 -0300

    quebra de linha e correção de casas decimais

--- a1 trab.c
+++ a1 trab.c
@@ -3,33 +3,40 @@ main()
 {
   float var1, var2, result;
   int opc;
-  printf ("Calculadora Setor Financeiro  ");
+  printf("\nCalculadora Setor Financeiro\n  ");
 
-  printf ("Operacao 1 - Soma  ");
+  printf("\nOperacao 1 - Soma\n  ");
 
-  printf ("Operacao 2 - Subtracao  ");
+  printf("\nOperacao 2 - Subtracao\n  ");
 
-  printf ("Operacao 3 -  Multiplicacao  ");
+  printf("\nOperacao 3 -  Multiplicacao\n  ");
 
-  printf ("Operacao 4 - Divisao ");
+  printf("\nOperacao 4 - Divisao\n ");
 
-  printf ("Escolha uma opcao de calculo");
-  scanf ("%d", &opc);
+  printf("\nEscolha uma opcao de calculo\n");
+  scanf("%d", &opc);
 
-  printf ("Digitar o primeiro valor");
-    scanf ("%f", &var1);
+  printf("Digitar o primeiro valor");
+  scanf("%f", &var1);
 
-  printf ("Digitar o segundo valor");
-    scanf ("%f", &var2);
+  printf("Digitar o segundo valor");
+  scanf("%f", &var2);
 
-if (opc == 1)
-  result = var1 + var2;
-if (opc == 2)
-  result = var1 - var2;
-if (opc == 3)
-  result = var1 * var2;
-if (opc == 4)
-  result = var1 / var2;
+  if (opc == 1)
+    result = var1 + var2;
+  if (opc == 2)
+    result = var1 - var2;
+  if (opc == 3)
+    result = var1 * var2;
+  if (opc == 4)
+    result = var1 / var2;
 
-printf ("O resultado final da operacao e: %f", result);
+  if (opc == 4)
+  {
+    printf("O resultado final da operacao e: %0.1f", result);
+  }
+  else
+  {
+    printf("O resultado final da operacao e: %0.0f", result);
+  }
 }
